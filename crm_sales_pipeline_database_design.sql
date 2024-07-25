275d006c5df92e4b7d0e76180d73b94d85ff0f00
jeyaram11 <jey.wkit@gmail.com>
2024-07-26 00:18:32 +0100

    Hello_world

---
+++ crm_sales_pipeline_database_design.sql
@@ -0,0 +1,15 @@
+--create staging schema, contains all load tables
+CREATE SCHEMA crm_sales_pipeline_staging;  
+
+--create datawarhouse, contains all transformed data
+CREATE SCHEMA crm_sales_pipeline_warehouse;
+
+
+
+
+
+
+
+
+
+
