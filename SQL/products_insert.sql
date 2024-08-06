4127b755d2ea6ed4ec09b1acb086636e7fe8cb68
jeyaram11 <jey.wkit@gmail.com>
2024-08-06 20:40:15 +0100

    added sql logic to keep a history of each record if records

--- SQL/products_insert.sql
+++ SQL/products_insert.sql
@@ -13,12 +13,11 @@ INSERT INTO crm_sales_pipeline_warehouse.products
 --update the records that already exist but need to be updated
 UPDATE crm_sales_pipeline_warehouse.products p
 SET
-  p.end_date = current_date
+  end_date = current_date
 FROM
   crm_sales_pipeline_staging.products_update u
 WHERE
   p.product_id = u.product_id AND p.end_date IS NOT NULL;
-
 
 --insert the new record from the update table
 INSERT INTO crm_sales_pipeline_warehouse.products
